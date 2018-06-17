e5e616d680d672acaff47f59090cffce330bd75f
DCoZdTCU2i0 <max_jeep@protonmail.com>
2018-06-17 17:48:48 -0400

    Added statistics endpoint

--- server.go
+++ server.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/DCoZdTCU2i0/jumpcloud/encoding"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
+
+var count *uint64 = new(uint64)
+var totalTime *uint64 = new(uint64)
 
 func hashHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -26,10 +31,33 @@ func hashHandler(w http.ResponseWriter, r *http.Request) {
 		close(done)
 	}()
 
+	start := time.Now()
 	outputPassword := encoding.Conversion(inputPassword)
+	elapsed := time.Since(start)
+
+	atomic.AddUint64(totalTime, uint64(elapsed.Nanoseconds()/1000000))
+	atomic.AddUint64(count, 1)
+
 	fmt.Fprint(w, outputPassword)
 
 	<-done
+}
+
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+	type Stats struct {
+		Total   uint64 `json:"total"`
+		Average uint64 `json:"average"`
+	}
+
+	var average uint64 = 0
+	if *count != 0 {
+		average = (*totalTime / *count)
+	}
+
+	output := Stats{*count, average}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(output)
 }
 
 func main() {
@@ -39,6 +67,7 @@ func main() {
 	temp := make(chan struct{})
 
 	http.HandleFunc("/hash", hashHandler)
+	http.HandleFunc("/stats", statsHandler)
 	http.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Shutting down server now.")
 
